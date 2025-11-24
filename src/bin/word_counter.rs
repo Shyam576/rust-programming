2af72937164ded8038995da3c05cf0a0079cc9e0
Shyam Basnet <shyambasnet289@gmail.com>
2025-11-24 16:50:44 +0600

    :tada: Word counter

--- src/bin/word_counter.rs
+++ src/bin/word_counter.rs
@@ -1,5 +1,12 @@
 use std::io;
 
-fn main(){
+fn main() {
+    println!(" Enter your text: ");
 
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    let count = input.split_whitespace().count();
+
+    println!("Word count : {}", count);
 }
